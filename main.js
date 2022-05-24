ae557069a1d06896e9c6659e266d2d7e6fcf918c
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-05-24 15:24:30 +0200

    react static version added

--- main.js
+++ main.js
@@ -1,34 +1,21 @@
-"use strict";
-
-document.addEventListener("DOMContentLoaded", initTypedJs());
-
-// ---------- typed.js ---------- //
-function initTypedJs() {
-  new Typed(".typed", {
-    strings: ["Rasmus Cederdorff.", "a Freelancer.", "a Lecturer.", "a Web Developer.", "an App Developer.", "a Web Architect.", "a Teacher."],
-    typeSpeed: 100,
-    loop: true,
-  });
-};
-
-// ---------- smooth scroll ---------- //
-
 // When the user scrolls down 500px from the top of the document, show the button
 window.onscroll = function () {
-  scrollFunction()
+    watchScroll();
 };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    document.getElementById("scrollTop").style.display = "block";
-  } else {
-    document.getElementById("scrollTop").style.display = "none";
-  }
+function watchScroll() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("scrollTop").style.display = "block";
+    } else {
+        document.getElementById("scrollTop").style.display = "none";
+    }
 }
 
 function scrollToTheTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 }
+
+document.querySelector("#scrollTop").addEventListener("click", scrollToTheTop);
