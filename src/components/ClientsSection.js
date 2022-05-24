3bcaf38822d36a9459175244e7878b1a3d1b170d
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-05-24 16:59:42 +0200

    Static replaced with react version

---
+++ src/components/ClientsSection.js
@@ -0,0 +1,25 @@
+import ClientRow from "./ClientRow";
+import { useEffect, useState } from "react";
+
+export default function ClientsSection() {
+    const [clients, setClients] = useState([]);
+
+    useEffect(() => {
+        async function getData() {
+            const response = await fetch("/data/clients.json");
+            const data = await response.json();
+            setClients(data);
+        }
+        getData();
+    }, []);
+    return (
+        <section id="clients">
+            <div className="headline-container">
+                <h2>Clients</h2>
+            </div>
+            {clients.map(client => (
+                <ClientRow client={client} key={client.id} />
+            ))}
+        </section>
+    );
+}
