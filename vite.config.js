fdade7681ad156504556fa892257b62d3bcdabdc
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-05-31 11:31:02 +0200

    chore: Update vite.config.js to remove commented code

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig(({ command }) => {
     base: "/"
   };
 
-  if (command !== "serve") {
-    config.base = "/cederdorff.github.io-v2/";
-  }
+  // if (command !== "serve") {
+  //   config.base = "/cederdorff.github.io/";
+  // }
 
   return config;
 });
