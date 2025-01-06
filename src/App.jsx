8bd8cd71b9e6745e8836367a1e286c26ad1ccb7e
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-01-06 22:26:14 +0100

    feat: Add LinkTree page and routing, including styling for the new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
+import LinkTree from "./pages/LinkTree";
 
 function App() {
   return (
     <Routes>
       {/* Routes go here */}
       <Route path="/" element={<Home />} />
+      <Route path="/links" element={<LinkTree />} />
     </Routes>
   );
 }
---
+++ src/pages/LinkTree.jsx
@@ -0,0 +1,12 @@
+import SocialIcons from "../components/SocialIcons";
+
+export default function LinkTree() {
+  return (
+    <section className="link-tree">
+      <figure>
+        <img src="img/race.webp" alt="Rasmus Cederdorff" />
+      </figure>
+      <SocialIcons />
+    </section>
+  );
+}
