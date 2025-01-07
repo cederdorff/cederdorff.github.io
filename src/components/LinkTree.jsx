0e9524dd7c47de9be874f52b54a76cda9d60e4a7
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-01-07 09:08:01 +0100

    refactor: Remove unnecessary target and rel attributes from LinkTree links

--- src/components/LinkTree.jsx
+++ src/components/LinkTree.jsx
@@ -1,50 +1,36 @@
 export default function LinkTree() {
   return (
     <article className="link-tree">
-      <a
-        href="https://houseofvincent.com/"
-        target="_blank"
-        rel="noreferrer"
-        title="This is a link to House of Vincent">
+      <a href="https://houseofvincent.com/" title="This is a link to House of Vincent">
         <i className="ion-ios-bolt"></i>
         House of Vincent
       </a>
       <a
         href="https://cederdorff.com/"
-        target="_blank"
-        rel="noreferrer"
         title="This is a link to my Website, Cederdorff.com">
         <i className="ion-ios-world"></i>
         Website
       </a>
       <a
         href="https://www.instagram.com/cederdorff/"
-        target="_blank"
-        rel="noreferrer"
         title="This is a link to my Instagram Account, Cederdorff">
         <i className="ion-social-instagram"></i>
         Instagram
       </a>
       <a
         href="https://www.linkedin.com/in/cederdorff/"
-        rel="noreferrer"
-        target="_blank"
         title="This is a link to my LinkedIn Account, Cederdorff">
         <i className="ion-social-linkedin"></i>
         LinkedIn
       </a>
       <a
         href="https://github.com/cederdorff"
-        rel="noreferrer"
-        target="_blank"
         title="This is a link to my GitHub Account, Cederdorff">
         <i className="ion-social-github"></i>
         GitHub
       </a>
       <a
         href="https://www.facebook.com/rcederdorff/"
-        rel="noreferrer"
-        target="_blank"
         title="This is a link to my Facebook Account, Cederdorff">
         <i className="ion-social-facebook"></i>
         Facebook
