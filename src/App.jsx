d727204fc7daaf70ca59f85d1eee707455af23d3
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-08-30 14:39:40 +0200

    refactor: Remove ApplicationPage and CVPage components and their routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Links from "./pages/Links";
-import ApplicationPage from "./pages/ApplicationPage";
-import CVPage from "./pages/CVPage";
 
 function App() {
   return (
@@ -10,8 +8,6 @@ function App() {
       {/* Routes go here */}
       <Route path="/" element={<Home />} />
       <Route path="/links" element={<Links />} />
-      <Route path="/application" element={<ApplicationPage />} />
-      <Route path="/cv" element={<CVPage />} />
     </Routes>
   );
 }
