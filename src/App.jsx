868adc852aa51cc6bd5d41d50cc5aadd9815023e
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-08-30 14:37:50 +0200

    feat: Add routes for ApplicationPage and CVPage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Links from "./pages/Links";
+import ApplicationPage from "./pages/ApplicationPage";
+import CVPage from "./pages/CVPage";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       {/* Routes go here */}
       <Route path="/" element={<Home />} />
       <Route path="/links" element={<Links />} />
+      <Route path="/application" element={<ApplicationPage />} />
+      <Route path="/cv" element={<CVPage />} />
     </Routes>
   );
 }
