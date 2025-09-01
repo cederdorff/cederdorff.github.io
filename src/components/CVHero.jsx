61f73f3e7422031e2497755df67a766567864704
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-09-01 10:57:23 +0200

    feat: Add CVHero component with smooth scroll functionality and links

---
+++ src/components/CVHero.jsx
@@ -0,0 +1,34 @@
+export default function CVHero() {
+  function scrollToContact(event) {
+    event.preventDefault();
+    const element = document.querySelector("#contact");
+    element.scrollIntoView({
+      behavior: "smooth"
+    });
+  }
+
+  return (
+    <section className="hero">
+      <article>
+        <h2>
+          Do you
+          <br /> need more?
+        </h2>
+        <p>
+          Download my{" "}
+          <a href="/CV-2024.pdf" rel="noreferrer" target="_blank">
+            curriculum vitae here
+          </a>
+          ,<br /> visit my{" "}
+          <a href="https://www.instagram.com/cederdorff/" rel="noreferrer" target="_blank">
+            Instragram Profile{" "}
+          </a>
+          or let&apos;s{" "}
+          <a href="/#contact" onClick={scrollToContact}>
+            get in touch
+          </a>
+        </p>
+      </article>
+    </section>
+  );
+}
