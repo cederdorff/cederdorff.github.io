4233766507b63b0a5c5f8b42bd77a1be94590423
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-09-03 09:13:45 +0200

    fix: remove basename from BrowserRouter for consistent routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/cederdorff.github.io/"}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
